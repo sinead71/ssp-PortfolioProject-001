8b894faeefa63eff05b4cfe09fc2a71ed6121553
sinead.71 <cooneysinead71.com>
2016-04-12 13:10:53 +0100

    Adding files. Not sure what was updated.

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', multer({storage: storageMethod}).any());
 app.use('/', routes);
 app.use('/users', users);
 
